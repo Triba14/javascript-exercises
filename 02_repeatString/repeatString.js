b09ad90e2c1e5a18c8ef66393312b2e9320a4f75
triba <giuseppet100@gmail.com>
2024-05-22 12:13:12 +0200

    finished exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
+let repeatString = function(word, times) {
+    if (times < 0) return "ERROR"
+    let str = "";
+    for (let i = 1; i <= times; i++) {
+        str += word
+    }
+    return str;
+}
 
-};
 
 // Do not edit below this line
 module.exports = repeatString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,6 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(array, ...elementToRemove) {
+    return array.filter(item => !elementToRemove.includes(item));
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
