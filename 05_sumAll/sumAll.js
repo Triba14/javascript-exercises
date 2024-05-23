428d3e5a4276c3f557ff925aaf24cb3886a0cd00
triba <giuseppet100@gmail.com>
2024-05-24 00:24:39 +0200

    finished exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(startNumber, finishNumber) {
+    if (startNumber < 0 || finishNumber < 0) return "ERROR";
+    if (!Number.isInteger(startNumber) || !Number.isInteger(finishNumber)) return "ERROR";
+    if (startNumber > finishNumber) {
+        const temp = startNumber;
+        startNumber = finishNumber;
+        finishNumber = temp;
+      }
+    
+    let sum = 0;
+    for (let i = startNumber; i <= finishNumber; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
